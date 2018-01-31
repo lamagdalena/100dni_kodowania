0bf96b4d6fb6ed28376bdcc6bb5fa2c5e3fd8df7
lamagdalena <35333893+lamagdalena@users.noreply.github.com>
2018-01-31 21:33:18 +0100

    Update dzien15_palindrome.py

--- dzien15_palindrome.py
+++ dzien15_palindrome.py
@@ -1,5 +1,6 @@
 def check_if_palindrome():
     word = input('Your word:')
+    word = word.lower()
     word_reversed = word[::-1]
     if word_reversed == word:
         return 'Your word {} is a palindrome'.format(word)
